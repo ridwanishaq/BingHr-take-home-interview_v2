d2d6e55916e9bee91a609f74425f03a8f66f36c2
ridwan <ridwanishaq2020@gmail.com>
2022-05-23 02:17:29 +0100

    Generated Authentication Scaffolding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('employeeID');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('mobile_no')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
